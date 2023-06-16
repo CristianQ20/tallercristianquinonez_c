37050fc723f50c146671e6e20f9c0e04feeadcf5
CristianQ20 <136030667+CristianQ20@users.noreply.github.com>
2023-06-16 01:42:04 +0000

    implementación de módulos

---
+++ programa.py
@@ -0,0 +1,8 @@
+from matematica import sumar, restar, multiplicar, dividir
+from geometria import distancia
+
+print(sumar(2,3))
+print(restar(4,2))
+print(multiplicar(3,2))
+print(dividir(36,6))
+print("Distancia:", distancia(1,2,5,6))
